1df1df55376bb9595037210124d812a6f1021463
Dimitris Miras <jimiras10@gmail.com>
2020-11-28 15:02:22 +0200

    Moiras Menu file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carousel-pause").click(function(){
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carousel-pause").click(function () {
         $("#mycarousel").carousel('pause');
     });
-    $("#carousel-play").click(function(){
+    $("#carousel-play").click(function () {
         $("#mycarousel").carousel('cycle');
     });
 });
+
